161ad0fa3523660d9006d211ff40f7a766b945e4
willo <willoleary6@gmail.com>
2020-05-10 02:12:32 +0100

    Ok, figured out a way to use the android emulator in conjunction with the email scanner, we just need to move the current email to be in the InFocus folder, can keep everything on a specific location track then for simulating screen taps in the android emulator.

--- adb command list.sh
+++ adb command list.sh
@@ -1,3 +1,18 @@
-cd 'C:\adb\platform-tools'
+cd C:\adb\platform-tools
+adb connect localhost:5555
 adb shell
+pm list packages -f
+monkey -p com.google.android.gm -v 1
 monkey -p com.daft.ie -c android.intent.category.LAUNCHER 1
+
+input touchscreen tap 500 600
+
+input touchscreen tap 200 100 # open folders
+
+input touchscreen swipe 200 1000 200 400 # swipe the options to bring the inFocus
+
+input touchscreen tap 200 600 # tap inFocus label
+
+input touchscreen tap 500 100 # close the folder button
+
+input touchscreen tap 1000 1000 # open daft ad
